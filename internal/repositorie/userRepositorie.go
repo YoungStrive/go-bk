84f5d69c7e42898209a7813dc9677c56f40ae2c6
yang.lin <ylstrive@163.com>
2025-09-17 16:07:37 +0800

    添加根据name 查询数据量的方法

--- internal/repositorie/userRepositorie.go
+++ internal/repositorie/userRepositorie.go
@@ -5,15 +5,22 @@ import (
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var db *gorm.DB
 
 func SetDB(db *gorm.DB) {
-	DB = db
+	db = db
 }
 
 func CreateUser(user *model.User) error {
-	if err := DB.Create(user).Error; err != nil {
+	if err := db.Create(user).Error; err != nil {
 		return err
 	}
 	return nil
+}
+
+// 根据用户名查找数据判断是否重名
+func GetCountByUserName(name string) int64 {
+	var nameCount int64
+	db.Model(&model.User{}).Where(&model.User{Name: name}).Count(&nameCount)
+	return nameCount
 }
